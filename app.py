ef633051261b3d25aaed741826aacd7b1e33ceb4
Ivan Kirika <ivtiks@vk.com>
2025-06-05 12:55:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Выбор 1-го вопроса
-    q1_discipline = 'Конструирование элементов гибких производственных систем'
+    # Вопрос 1: Автоматизированные транспортные + Конструирование элементов
+    group1 = [
+        'Автоматизированные транспортные и накопительные системы',
+        'Конструирование элементов гибких производственных систем'
+    ]
+    q1_discipline = random.choice(group1)
     q1 = random.choice(questions[q1_discipline])
     q1_number = q1.split('. ')[0]
     q1_text = q1[len(q1_number) + 2:]
 
-    # Выбор 2-го вопроса с указанием дисциплины
-    q2_disciplines = [
+    # Вопрос 2: Наладка и программирование ЧПУ
+    group2 = [
         'Наладка станков с числовым программным управлением',
         'Программирование станков с числовым программным управлением'
     ]
-    q2_discipline = random.choice(q2_disciplines)
+    q2_discipline = random.choice(group2)
     q2 = random.choice(questions[q2_discipline])
     q2_number = q2.split('. ')[0]
     q2_text = q2[len(q2_number) + 2:]
 
-    # Выбор 3-го вопроса
-    excluded_disciplines = [q1_discipline] + q2_disciplines
-    other_disciplines = [d for d in questions if d not in excluded_disciplines]
-    q3_discipline = random.choice(other_disciplines)
+    # Вопрос 3: Программирование роботов и ПЛК
+    group3 = [
+        'Программирование промышленных роботов',
+        'Программирование промышленных контроллеров'
+    ]
+    q3_discipline = random.choice(group3)
     q3 = random.choice(questions[q3_discipline])
     q3_number = q3.split('. ')[0]
     q3_text = q3[len(q3_number) + 2:]
